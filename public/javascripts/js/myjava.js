3c18155367ab38415d30c3d4a9cf9b63a2922efb
QuanZhong WU <wuquanzhong@yahoo.com>
2017-09-29 15:39:45 +0800

    修改：     public/javascripts/js/myjava.js

--- public/javascripts/js/myjava.js
+++ public/javascripts/js/myjava.js
@@ -74,11 +74,5 @@
                 });
 
 
-                /* Magnific pop up
-                ------------------------- */
-                $('.tm-img-grid').magnificPopup({
-                    delegate: 'a', // child items selector, by clicking on it popup will open
-                    type: 'image',
-                    gallery: {enabled:true}
-                });
+              
             });
